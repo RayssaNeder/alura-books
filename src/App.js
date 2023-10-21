4d190284922eeaf9a865bad121ac429f3ad69d40
RayssaNeder <rayssafreitas13@gmail.com>
2023-10-21 08:38:23 -0300

    Moving logo to a new component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from './imagens/logo.svg'
+import Logo from './componentes/Logo'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className='logo'>
-          <img src={logo} alt='logo'></img>
-          <p><strong>Alura Books</strong></p>
-        </div>
+        <Logo></Logo>
       </header>
     </div>
   );
