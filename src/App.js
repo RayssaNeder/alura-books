fd9cfe2330500f092d169b54033863c3946947de
RayssaNeder <rayssafreitas13@gmail.com>
2023-10-25 15:47:51 -0300

    Update: include global style

--- src/App.js
+++ src/App.js
@@ -7,10 +7,6 @@ const AppContainer = styled.div`
   height: 100vh;
   background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
 
-
-  li{
-    list-style: none;
-  }
 `
 
 function App() {
