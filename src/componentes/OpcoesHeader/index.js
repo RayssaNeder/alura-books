7ad5866643989e1650b7fd18e69c9cea8dde735b
RayssaNeder <rayssafreitas13@gmail.com>
2023-11-12 21:36:01 -0300

    Include route to header options

--- src/componentes/OpcoesHeader/index.js
+++ src/componentes/OpcoesHeader/index.js
@@ -1,4 +1,5 @@
 
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const OpcoesContainer= styled.ul`
@@ -16,13 +17,13 @@ const Opcao = styled.li`
     cursor: pointer;
     text-align: center;
 `
-const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'ESTANTE']
 
 function OpcoesHeader(){
     return(
         <OpcoesContainer>
          { textoOpcoes.map((texto) => (
-          <Opcao><p>{texto}</p></Opcao>
+            <Link to={`/${texto.toLowerCase()}`}><Opcao><p>{texto}</p></Opcao></Link> 
         ) ) }
         </OpcoesContainer>
     )
